59d64df03d37c6a3ea0c0c2316cdf35a81dfab68
SpherosGames <jbol1@st.noorderpoort.nl>
2025-01-24 20:37:58 +0100

    Enemy finished

--- Assets/Main/Scripts/Enemy.cs
+++ Assets/Main/Scripts/Enemy.cs
@@ -7,16 +7,56 @@ namespace Bubble
         [SerializeField] private Transform target;
         [SerializeField] private Rigidbody2D rb;
         [SerializeField] private float speed;
+        [SerializeField] private float maxSpeed;
         [SerializeField] private Vector2 dashDir;
         [SerializeField] private float dashForce;
+        [SerializeField] private float dashTime;
+
+        private float dashTimer;
+        private bool isDashing;
+
+        private void OnEnable()
+        {
+            dashTimer = dashTime;
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.CompareTag("Bullet"))
+            {
+                Dash();
+            }
+        }
 
         private void Update()
         {
-            Movement();
+            //print(isDashing);
 
-            if (Input.GetKeyDown(KeyCode.E))
+            if (isDashing)
             {
-                Dash();
+                dashTimer -= Time.deltaTime;
+
+                if (dashTimer <= 0)
+                {
+                    dashTimer = dashTime;
+                    isDashing = false;
+                }
+            }
+
+            //if (Input.GetKeyDown(KeyCode.E))
+            //{
+            //    isDashing = true;
+            //    Dash();
+            //}
+        }
+
+        private void FixedUpdate()
+        {
+            if (!isDashing)
+            {
+                Movement();
+
+                rb.linearVelocity = Vector2.ClampMagnitude(rb.linearVelocity, maxSpeed);
             }
 
             //agent.SetDestination(target.position);
@@ -25,14 +65,14 @@ namespace Bubble
         private void Dash()
         {
             print("Dash!");
-            rb.AddForce(dashDir * dashForce, ForceMode2D.Impulse);
+            rb.linearVelocity = dashDir * dashForce;
         }
 
         private void Movement()
         {
             Vector3 moveDir = target.position - transform.position;
 
-            moveDir.Normalize();
+            //moveDir.Normalize();
 
             rb.linearVelocity = moveDir * speed;
         }
