7ac938fbcf374b02c87dc7037852920fc22910f7
SpherosGames <jbol1@st.noorderpoort.nl>
2025-01-24 22:28:18 +0100

    Enemy

---
+++ Assets/Main/Scripts/Enemy.cs
@@ -0,0 +1,97 @@
+using UnityEngine;
+
+namespace Bubble
+{
+    public class Enemy : MonoBehaviour
+    {
+        [SerializeField] private Transform target;
+        [SerializeField] private Rigidbody2D rb;
+        [SerializeField] private float speed;
+        [SerializeField] private float maxSpeed;
+        [SerializeField] private Vector2 dashDir;
+        [SerializeField] private float dashForce;
+        [SerializeField] private float dashTime;
+        [SerializeField] private float deathDelay = 1;
+
+        private float dashTimer;
+        private bool isDashing;
+
+        private void OnEnable()
+        {
+            dashTimer = dashTime;
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.CompareTag("Bullet"))
+            {
+                Dash();
+            }
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.CompareTag("Bullet"))
+            {
+                Invoke(nameof(Death), deathDelay);
+            }
+        }
+
+        private void Death()
+        {
+            Destroy(gameObject);
+        }
+
+        private void Update()
+        {
+            //print(isDashing);
+
+            if (isDashing)
+            {
+                dashTimer -= Time.deltaTime;
+
+                if (dashTimer <= 0)
+                {
+                    print("Nah dashing");
+                    dashTimer = dashTime;
+                    isDashing = false;
+                }
+            }
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Dash();
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            if (!isDashing)
+            {
+                Movement();
+
+                rb.linearVelocity = Vector2.ClampMagnitude(rb.linearVelocity, maxSpeed);
+            }
+
+            //agent.SetDestination(target.position);
+        }
+
+        private void Dash()
+        {
+            isDashing = true;
+            print("Dash!");
+            rb.linearVelocity = dashDir * dashForce;
+        }
+
+        private void Movement()
+        {
+            if (!target) return;
+
+            Vector3 moveDir = target.position - transform.position;
+
+            //moveDir.Normalize();
+
+            rb.linearVelocity = moveDir * speed;
+        }
+    }
+}
