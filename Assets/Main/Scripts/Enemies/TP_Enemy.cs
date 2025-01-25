37a5863a16dbd536633bde695362d418a54e2023
Niek Melet <niek+git@meletstudios.com>
2025-01-25 09:18:09 +0100

    it worked

--- Assets/Main/Scripts/Enemies/TP_Enemy.cs
+++ Assets/Main/Scripts/Enemies/TP_Enemy.cs
@@ -1,9 +1,44 @@
+using Bubble.Temp;
 using UnityEngine;
 
 namespace Bubble.Enemies
 {
     public class TP_Enemy : GenericAhEnemy
     {
+        [SerializeField] private Transform playerFirePoint;
+        [SerializeField] private float teleportationRange;
         
+        private bool teleporting;
+
+        protected override void OnValidate()
+        {
+            base.OnValidate();
+
+            playerFirePoint = target.GetComponent<PC_TopDown>().GetComponentInChildren<PlayerShoot>().transform;
+        }
+
+        protected override void Start()
+        {
+            base.Start();
+            playerFirePoint = target.GetComponent<PC_TopDown>().GetComponentInChildren<PlayerShoot>().transform;
+        }
+
+        protected override void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag("Bullet"))
+            {
+                teleporting = true;
+                TeleportBehindTarget();
+            }
+        }
+        
+        private void TeleportBehindTarget()
+        {
+            Vector2 firePointPosition = playerFirePoint.position;
+            Vector2 backDirection = playerFirePoint.right * teleportationRange;
+            
+            Vector2 newPos = firePointPosition + backDirection;
+            transform.position = newPos;
+        }
     }
 }
