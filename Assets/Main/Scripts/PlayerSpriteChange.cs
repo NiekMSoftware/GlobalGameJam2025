75b99cf2afa34b96e1dfa43a942d9e7bf1de156b
Spectre <rpverschuren@st.noorderpoort.nl>
2025-01-25 17:40:18 +0100

    Player changes sprite based on movement

---
+++ Assets/Main/Scripts/PlayerSpriteChange.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+namespace Bubble
+{
+    public class PlayerSpriteChange : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody2D rb;
+        [SerializeField] private SpriteRenderer player;
+        [SerializeField] private Sprite[] faces;
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            print(rb.linearVelocity + "Hi");
+
+            if(rb.linearVelocity != Vector2.zero)
+            {
+                ChangeSprite();
+            }
+        }
+
+        private void ChangeSprite()
+        {
+            if(rb.linearVelocity.x < -3)
+            {
+                player.sprite = faces[0];
+            }
+            if (rb.linearVelocity.x > 3)
+            {
+                player.sprite = faces[1];
+            }
+            if (rb.linearVelocity.y < -3)
+            {
+                player.sprite = faces[2];
+            }
+            if (rb.linearVelocity.y > 3)
+            {
+                player.sprite = faces[3];
+            }
+        }
+    }
+}
