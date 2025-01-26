14f84c9aba47e7db5eb12234f2b7a3781a0a55e1
Marinus Retra <marinus@flowonline.nl>
2025-01-26 09:41:59 +0100

    Updated canvas prefab with reset on death menu

--- Assets/RestartButton.cs
+++ Assets/RestartButton.cs
@@ -6,8 +6,18 @@ namespace Bubble
     public class RestartButton : MonoBehaviour
     {
         public void RestartLevel()
-        { 
+        {
+            SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene());
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            SceneManager.SetActiveScene(SceneManager.GetSceneByBuildIndex(0));
+            Time.timeScale = 1.0f;
+        }
+
+        public void MainMenu()
+        {
+            SceneManager.LoadScene(1);
+            SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene());
+            SceneManager.SetActiveScene(SceneManager.GetSceneByBuildIndex(1));
         }
     }
 }
